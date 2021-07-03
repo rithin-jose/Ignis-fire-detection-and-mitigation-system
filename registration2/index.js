d75ae102f8ced410fc4a486d455f90e6e89b40af
unknown <rithinja@gmail.com>
2021-07-03 17:09:05 +0530

    Alert integrated

--- registration2/index.js
+++ registration2/index.js
@@ -1,9 +1,15 @@
+var bodyParser = require('body-parser');
+require('dotenv').config()
 var express = require('express')
 var app = express();
-var bodyParser = require('body-parser');
+const Twilio = require('twilio');
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
 app.use(express.static(__dirname+'\\public'));
 app.set('view engine','ejs');
 
@@ -20,6 +26,39 @@ app.get("/dashboard",(req,res) =>{
     res.render('desktop4')
 })
 
+app.get("/alert",(req,res) =>{
+    const accountSid = process.env.SID;
+    const authToken = process.env.Token;
+    
+
+    const client = new Twilio(accountSid, authToken);
+
+    const service = client.notify.services(process.env.Service);
+
+    const notioption = {
+        toBinding:[
+            JSON.stringify({'binding_type': 'sms', 'address': '+917012434015'}),
+            JSON.stringify({'binding_type': 'sms', 'address': '+918078493452'}),
+            JSON.stringify({'binding_type': 'sms', 'address': '+917907504373'}),
+            JSON.stringify({'binding_type': 'sms', 'address': '+918075876208'}),      
+        ],
+        body:"Fire in the building Evacute following the routing system"
+    }
+
+    service.notifications
+    .create(notioption)
+
+    .then(notification => {
+        console.log(notification);
+    })
+
+    .catch(error => {
+        console.log(error);
+    })
+    .done();
+    res.send("Done")
+    })
+
 app.listen(5500,() => {
-    console.log(`App started at port`)     
+    console.log(`App started at port`)
 })
